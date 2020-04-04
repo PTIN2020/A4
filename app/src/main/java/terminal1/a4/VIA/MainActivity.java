f23ff24a29c7c19e76006c0ee14a5ed62cb9f2f4
SergiSuarez <40240086+SergiSuarez@users.noreply.github.com>
2020-04-04 19:21:51 +0200

    cambio nombre login

--- app/src/main/java/terminal1/a4/VIA/MainActivity.java
+++ app/src/main/java/terminal1/a4/VIA/MainActivity.java
@@ -1,8 +1,10 @@
-package terminal1.a4.loginui;
+package terminal1.a4.VIA;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import terminal1.a4.loginui.R;
 
 public class MainActivity extends AppCompatActivity {
 
