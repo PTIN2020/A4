e5ea525080aba5ad8dd0f64fba033bd122234344
SergiSuarez <40240086+SergiSuarez@users.noreply.github.com>
2020-04-26 18:17:42 +0200

    Añadido activity negociofull

---
+++ app/src/main/java/terminal1/a4/loginui/NegocioFull.java
@@ -0,0 +1,14 @@
+package terminal1.a4.loginui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NegocioFull extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_negocio_full);
+    }
+}
