5252c0eac39fe006d0c8d0d4f03d602bca2e19cc
Sergio <sergiandres32@gmail.com>
2020-05-16 13:19:15 +0200

    Pedir vehiculo V2 No discrimino a los no discapacitados

--- app/src/main/java/terminal1/a4/loginui/servicios.java
+++ app/src/main/java/terminal1/a4/loginui/servicios.java
@@ -3,7 +3,6 @@ package terminal1.a4.loginui;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
