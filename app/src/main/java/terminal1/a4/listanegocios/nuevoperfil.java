644bcb8e508e89e0538bd88006258f8ba36c6ac8
Sergio <sergiandres32@gmail.com>
2020-05-30 11:40:15 +0200

    Shared preferences Login y registro funcional

--- app/src/main/java/terminal1/a4/listanegocios/nuevoperfil.java
+++ app/src/main/java/terminal1/a4/listanegocios/nuevoperfil.java
@@ -1,7 +1,5 @@
 package terminal1.a4.listanegocios;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
@@ -9,46 +7,198 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import terminal1.a4.loginui.R;
+import terminal1.a4.loginui.servicios;
+import terminal1.a4.tarjeta_embarque.Tembarque;
+
 
 public class nuevoperfil extends AppCompatActivity {
     Dialog popup;
+    private RequestQueue mQueue;
+
+//Abandone toda esperanza aquel que ose leer mi código espagueti
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevoperfil);
         popup = new Dialog(this);
+        mQueue = Volley.newRequestQueue(this);
 
     }
 
+    public void regis(View v) {
+        final EditText[] camporepetirpassword = new EditText[1], camponombre = new EditText[1], campoapellidos = new EditText[1], campoanacio = new EditText[1], campopassword = new EditText[1], campousuario = new EditText[1], campoid_user = new EditText[1], campotelefono = new EditText[1];
+        ;
+        final Boolean[] disabled = new Boolean[1];
+        campopassword[0] = (EditText) findViewById(R.id.editpassword);
+        String password = campopassword[0].getText().toString();
+        camporepetirpassword[0] = (EditText) findViewById(R.id.edittpasword2);
+        String password2 = camporepetirpassword[0].getText().toString();
+        camponombre[0] = (EditText) findViewById(R.id.editnombre);
+        String nombre = camponombre[0].getText().toString();
+        campoapellidos[0] = (EditText) findViewById(R.id.editapellidos);
+        String apellidos = campoapellidos[0].getText().toString();
+        campoanacio[0] = (EditText) findViewById(R.id.editnacio);
+        String nacio = campoanacio[0].getText().toString();
+        campoid_user[0] = (EditText) findViewById(R.id.editid_user);
+        String iduser = campoid_user[0].getText().toString();
+        campotelefono[0] = (EditText) findViewById(R.id.edittelefono);
+        String telefono = campotelefono[0].getText().toString();
+        campousuario[0] = (EditText) findViewById(R.id.editusuario);
+        String usuario = campousuario[0].getText().toString();
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        RadioButton Masc = findViewById(R.id.GMasculino);
+        RadioButton Fem = findViewById(R.id.GFemenino);
+        RadioButton Otr = findViewById(R.id.GOtro);
+        String genero = "";
+        if (checkBox.isChecked()) {
+            disabled[0] = true;
+        } else {
+            disabled[0] = false;
+        }
+        if (Masc.isChecked()) {
+            genero = "Masculino";
+        }
+        if (Fem.isChecked()) {
+            genero = "Femenino";
+        }
+        if (Otr.isChecked()) {
+            genero = "Otro";
+        }
 
-    public void ShowPopUp(View v) {
-        TextView desc;
-        ImageView exclamation;
-        Button accept;
-        Button deny;
-        popup.setContentView(R.layout.advice);
-        accept = (Button) popup.findViewById(R.id.accept_policy);
-        deny = (Button) popup.findViewById(R.id.deny_policy);
-        desc = (TextView) popup.findViewById(R.id.Desc);
-        exclamation = (ImageView) popup.findViewById(R.id.advice);
-        accept.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(nuevoperfil.this, Perfil.class));
-            }
-        });
-        deny.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                popup.dismiss();
-            }
-        });
-        popup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        popup.show();
+        if (iduser.isEmpty() || nombre.isEmpty() || apellidos.isEmpty() || nacio.isEmpty() || genero.isEmpty() || telefono.isEmpty() || usuario.isEmpty() || password.isEmpty() || password2.isEmpty()) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(nuevoperfil.this);
+            builder.setMessage("Debes rellenar todos los campos");
+            builder.setCancelable(true);
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        } else if (password.equals(password2)) {
+            TextView desc;
+            ImageView exclamation;
+            Button accept;
+            Button deny;
+            popup.setContentView(R.layout.advice);
+            accept = (Button) popup.findViewById(R.id.accept_policy);
+            deny = (Button) popup.findViewById(R.id.deny_policy);
+            desc = (TextView) popup.findViewById(R.id.Desc);
+            exclamation = (ImageView) popup.findViewById(R.id.advice);
+            accept.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    camponombre[0] = (EditText) findViewById(R.id.editnombre);
+                    String nombre = camponombre[0].getText().toString();
+                    campoapellidos[0] = (EditText) findViewById(R.id.editapellidos);
+                    String apellidos = campoapellidos[0].getText().toString();
+                    campoanacio[0] = (EditText) findViewById(R.id.editnacio);
+                    String nacio = campoanacio[0].getText().toString();
+                    campoid_user[0] = (EditText) findViewById(R.id.editid_user);
+                    String iduser = campoid_user[0].getText().toString();
+                    campotelefono[0] = (EditText) findViewById(R.id.edittelefono);
+                    String telefono = campotelefono[0].getText().toString();
+                    campousuario[0] = (EditText) findViewById(R.id.editusuario);
+                    String usuario = campousuario[0].getText().toString();
+                    campopassword[0] = (EditText) findViewById(R.id.editpassword);
+                    String password = campopassword[0].getText().toString();
+                    CheckBox checkBox = findViewById(R.id.checkBox);
+                    RadioButton Masc = findViewById(R.id.GMasculino);
+                    RadioButton Fem = findViewById(R.id.GFemenino);
+                    RadioButton Otr = findViewById(R.id.GOtro);
+                    String genero = "";
+                    if (checkBox.isChecked()) {
+                        disabled[0] = true;
+                    } else {
+                        disabled[0] = false;
+                    }
+                    if (Masc.isChecked()) {
+                        genero = "Masculino";
+                    }
+                    if (Fem.isChecked()) {
+                        genero = "Femenino";
+                    }
+                    if (Otr.isChecked()) {
+                        genero = "Otro";
+                    }
+                    String url = "http://192.168.1.57:3000/pasajero";
+                    //mTextViewResult.setText("Todo Ready!");
+                    JSONObject pasajero = new JSONObject();
+                    try {
+                        pasajero.put("id_user", iduser);
+                        pasajero.put("nombre", nombre);
+                        pasajero.put("apellidos", apellidos);
+                        pasajero.put("nacio", nacio);
+                        pasajero.put("genero", genero);
+                        pasajero.put("telefono", telefono);
+                        pasajero.put("disable", disabled[0]);
+                        pasajero.put("username", usuario);
+                        pasajero.put("password", password);
+                        pasajero.put("vip", false);
+
+                    } catch (JSONException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                    //mTextViewResult.setText("Response: " + pasajero.toString());
+
+                    JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, pasajero, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            if (response.toString().contains("ok")) {
+                                startActivity(new Intent(nuevoperfil.this, Tembarque.class));
+                                finish();
+                            } else {
+                                popup.dismiss();
+                                final AlertDialog.Builder builder = new AlertDialog.Builder(nuevoperfil.this);
+                                builder.setMessage("La dirección de e-mail introducida ya está en uso");
+                                builder.setCancelable(true);
+                                AlertDialog alertDialog = builder.create();
+                                alertDialog.show();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            //si entra aqui esta bien ya que en el post no hay json.
+                        }
+                    });
+
+                    mQueue.add(jsonRequest);
+                }
+            });
+            deny.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    popup.dismiss();
+                }
+            });
+            popup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            popup.show();
+        } else {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(nuevoperfil.this);
+            builder.setMessage("Las contraseñas introducidas no coinciden");
+            builder.setCancelable(true);
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        }
     }
-
 }
--- app/src/main/java/terminal1/a4/loginui/Login.java
+++ app/src/main/java/terminal1/a4/loginui/Login.java
@@ -1,19 +1,62 @@
 package terminal1.a4.loginui;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import terminal1.a4.listanegocios.nuevoperfil;
 import terminal1.a4.tarjeta_embarque.Tembarque;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+
+import android.os.Bundle;
+
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import terminal1.a4.loginui.R;
+import terminal1.a4.loginui.mapa;
+import terminal1.a4.loginui.servicios;
+import terminal1.a4.tarjeta_embarque.Tembarque;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Login extends AppCompatActivity {
 
-
+    private RequestQueue mQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -22,6 +65,14 @@ public class Login extends AppCompatActivity {
 
         configureNextButton();
         configureregistrarusuario();
+        mQueue = Volley.newRequestQueue(this);
+        //estoy hay que pasarla a la splash screen
+        SharedPreferences preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+        String token = preferences.getString("username","");
+        if(!token.equals("")){
+            startActivity(new Intent(Login.this, Tembarque.class));
+            finish();
+        }
     }
 
     private void configureNextButton(){
@@ -29,10 +80,56 @@ public class Login extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Login.this, Tembarque.class));
+                jsonPost();
             }
         });
     }
+    private void jsonPost() {
+        final EditText[] campouser = new EditText[1], campopassword = new EditText[1];
+        campopassword[0] = (EditText) findViewById(R.id.contrasenya);
+        campouser[0] = (EditText) findViewById(R.id.correu);
+        String password = campopassword[0].getText().toString();
+        String user = campouser[0].getText().toString();
+
+        String url = "http://192.168.1.57:3000/login";
+        //mTextViewResult.setText("Todo Ready!");
+        JSONObject pasajero = new JSONObject();
+        try {
+            pasajero.put("correu", user);
+            pasajero.put("contrasenya", password);
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, pasajero, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                if(response.toString().contains("ok")){
+                    SharedPreferences preferences=getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor=preferences.edit();
+                    editor.putString("username",user);
+                    editor.commit();
+                    startActivity(new Intent(Login.this, Tembarque.class));
+                    finish();
+                }
+                else{
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+                    builder.setMessage("Email o Contraseña incorrectos");
+                    builder.setCancelable(true);
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //si entra aqui esta bien ya que en el post no hay json.
+            }
+        });
+        mQueue.add(jsonRequest);
+    }
+
     private void configureregistrarusuario(){
         Button registButton = (Button) findViewById(R.id.Registralogin);
         registButton.setOnClickListener(new View.OnClickListener() {
