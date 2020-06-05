cfe4ef3d40c93c9446fefb93247e2c88d3911ea2
Sergio <sergiandres32@gmail.com>
2020-06-05 19:02:13 +0200

    Splash screen funcional

--- app/src/main/java/terminal1/a4/loginui/StartActivity.java
+++ app/src/main/java/terminal1/a4/loginui/StartActivity.java
@@ -2,7 +2,13 @@ package terminal1.a4.loginui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+
+import terminal1.a4.tarjeta_embarque.Tembarque;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                SharedPreferences preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+                String token = preferences.getString("username","");
+                if(!token.equals("")){
+                    startActivity(new Intent(StartActivity.this, Tembarque.class));
+                    finish();
+                }
+                else{
+                    startActivity(new Intent(StartActivity.this, Login.class));
+                }
+            }
+        }, 1000);   //5 seconds
     }
 }
