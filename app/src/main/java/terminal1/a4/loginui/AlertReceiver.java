5b1a408913b1abc114487b5e6fbaa9f043505f1d
Sergio <sergiandres32@gmail.com>
2020-06-16 17:28:01 +0200

    Notifiacion cuando falten 30 minutos o menos para el vuelo

---
+++ app/src/main/java/terminal1/a4/loginui/AlertReceiver.java
@@ -0,0 +1,25 @@
+package terminal1.a4.loginui;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class AlertReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        SharedPreferences preferences = context.getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+        String embarque = preferences.getString("Horaembarque","");
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notifyembarque")
+                .setSmallIcon(R.drawable.logo_login)
+                .setContentTitle("Cierra de la puerta de embarque a las " + embarque + "h")
+                .setContentText("Quedan menos de 30 minutos para que cierre el embarque")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(200, builder.build());
+    }
+}
