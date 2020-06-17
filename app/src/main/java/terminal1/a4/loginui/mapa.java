eb08d971bc866d4b6416322888b8c88dc8111f72
1Woody <daniel.benavente4@gmail.com>
2020-06-17 21:46:40 +0200

    mapa

--- app/src/main/java/terminal1/a4/loginui/mapa.java
+++ app/src/main/java/terminal1/a4/loginui/mapa.java
@@ -6,9 +6,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MenuItem;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import terminal1.a4.listanegocios.ListaNegocios;
@@ -19,13 +28,18 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
+import android.view.View;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.BoundingBox;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
@@ -34,10 +48,16 @@ import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.OverlayItem;
 
 import java.util.ArrayList;
+import java.util.StringJoiner;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class mapa extends AppCompatActivity {
     private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
     private MapView map = null;
+    private RequestQueue mQueue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,9 +76,11 @@ public class mapa extends AppCompatActivity {
         //tile servers will get you banned based on this string
 
         //inflate and create the map
+
         setContentView(R.layout.activity_mapa);
 
         map = (MapView) findViewById(R.id.map);
+        map.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         map.setTileSource(TileSourceFactory.MAPNIK);
         requestPermissionsIfNecessary(new String[] {
                 // if you need to show the current location, uncomment the line below
@@ -69,17 +91,63 @@ public class mapa extends AppCompatActivity {
         });
         map.setMultiTouchControls(true);
 
+        //pasajero
+        mQueue = Volley.newRequestQueue(this);
+
+
+        //periodic call
+        class UpdatePos extends TimerTask {
+            public void run() {
+                //jsonGet();
+                map.getOverlayManager().clear();
+                map.invalidate();
+                Points_map();
+                jsonGet();
+                jsonGetCoche();
+                // Change the overlay
+            }
+        }
+
+        Timer timer = new Timer();
+        timer.schedule(new UpdatePos(), 0, 10000);
+
         // geoloc palma
-        GeoPoint startPoint = new GeoPoint(39.54827, 2.73418);
+        GeoPoint StartPoint = new GeoPoint(39.54827, 2.73418);
+
         IMapController mapController = map.getController();
         //zoom aprox cerca
-        mapController.setZoom(18);
-        mapController.setCenter(startPoint);
+        mapController.setZoom(18.00);
+        mapController.setCenter(StartPoint);
 
+        Points_map ();
+        /*Thread t = new Thread() {
 
-        //add_points_map();
-        add_marker(startPoint);
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(3000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                map.getOverlayManager().clear();
+                                map.invalidate();
+                                Points_map();
+                                jsonGet();
+                                //SharedPreferences preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+                                //String ubi = preferences.getString("last_pos","");
+                                //String[] s = ubi.split(",");
+                                //GeoPoint ubiPoint = new GeoPoint(Double.parseDouble(s[0]), Double.parseDouble(s[1]));
+                                //add_marker_pospasajero(ubiPoint);
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
 
+        t.start();*/
         map.invalidate();
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
@@ -112,7 +180,163 @@ public class mapa extends AppCompatActivity {
         });
     }
 
+    private void Points_map (){
 
+        //supermercados
+        GeoPoint cartPoint = new GeoPoint(39.55032, 2.73625);
+        GeoPoint cart2Point = new GeoPoint(39.546894,2.733270);
+        GeoPoint cart3Point = new GeoPoint(39.54994, 2.72821);
+
+        //restaurantes
+        GeoPoint restaurantPoint= new GeoPoint(39.55069, 2.73);
+        GeoPoint restaurant2Point = new GeoPoint(39.5487, 2.73388);
+        GeoPoint restaurant3Point = new GeoPoint(39.5498, 2.7356);
+
+        //info
+        GeoPoint infoPoint= new GeoPoint(39.54859, 2.73179);
+        GeoPoint info2Point = new GeoPoint(39.54949, 2.73463);
+        GeoPoint info3Point = new GeoPoint(39.55023, 2.72913);
+
+        //vip
+        GeoPoint vipPoint= new GeoPoint(39.54508, 2.73123);
+        GeoPoint vip2Point = new GeoPoint(39.54528, 2.73174);
+        GeoPoint vip3Point = new GeoPoint(39.54578, 2.73274);
+        GeoPoint vip4Point = new GeoPoint(39.54606, 2.73327);
+
+        add_marker_cart(cartPoint, "Duty Free");
+        add_marker_cart(cart2Point, "Vilanova Souvenirs");
+        add_marker_cart(cart3Point, "Adidas");
+
+        //restuarants
+        add_marker_restaruant(restaurantPoint, "Tagliatella");
+        add_marker_restaruant(restaurant2Point, "Restaurant Pepin");
+        add_marker_restaruant(restaurant3Point, "Mc Donald's");
+
+        //info
+        add_marker_info(infoPoint, "Punt d'informació P");
+        add_marker_info(info2Point, "Punt d'informació C");
+        add_marker_info(info3Point, "Punt d'informació A");
+
+        //vip
+        add_marker_vip(vipPoint, "Sala VIP Clàssica");
+        add_marker_vip(vip2Point, "Sala VIP Tropical");
+        add_marker_vip(vip3Point, "Sala VIP Mediterrània");
+        add_marker_vip(vip4Point, "Sala VIP Clàssica");
+
+    }
+
+    private void jsonGet(){
+        SharedPreferences preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+        String user = preferences.getString("username","");
+        String url = "http://craaxcloud.epsevg.upc.edu:36302/nodos/random/position/";
+        url += user;
+        //System.out.println(url);
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            double longitud = Double.parseDouble(response.getString("longitud"));
+                            double latitud =  Double.parseDouble(response.getString("latitud"));
+                            GeoPoint ubi = new GeoPoint(latitud, longitud);
+                            add_marker_pospasajero(ubi, "Mi posición actual");
+                            //SharedPreferences preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+                            //SharedPreferences.Editor editor = preferences.edit();
+                            //editor.putString("last_pos", response.getString("longitud")+","+response.getString("latitud"));
+                            //editor.apply();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                //mTextViewResult.setText(error.getMessage());
+            }
+        });
+        mQueue.add(request);
+    }
+
+
+    private void jsonGetCoche(){
+        SharedPreferences preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+        String user = preferences.getString("username","");
+        String url = "http://craaxcloud.epsevg.upc.edu:36302/cochesuser/";
+        url += user;
+        //System.out.println(url);
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String node = null;
+                            node = response.getString("puntActual");
+                            if (!node.equals("")) jsonGetResolvNode(node);
+                            //jsonGetResolvNode(node);
+                            //GeoPoint ubi_coche = new GeoPoint(latitud, longitud);
+                            //add_marker_pospasajero(ubi_coche);
+                            //jsonGet();
+                            //add_marker_poscoche(ubi_coche);
+                            //SharedPreferences preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+                            //SharedPreferences.Editor editor = preferences.edit();
+                            //editor.putString("last_pos", response.getString("longitud")+","+response.getString("latitud"));
+                            //editor.apply();
+
+                        } catch (JSONException e) {
+                            //System.out.println("No hay coche");
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                //mTextViewResult.setText(error.getMessage());
+            }
+        });
+        mQueue.add(request);
+    }
+
+    private void jsonGetResolvNode(String node){
+        //SharedPreferences preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+        //String user = preferences.getString("username","");
+        String url = "http://craaxcloud.epsevg.upc.edu:36302/nodos/";
+        url += node;
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            //jsonGet();
+                            double latitud = Double.parseDouble(response.getString("longitud"));
+                            double longitud =  Double.parseDouble(response.getString("latitud"));
+                            //String longitud = response.getString("longitud");
+                            //String latitud =  response.getString("latitud");
+                            //add_marker_poscoche(point_test, latitud+","+longitud);
+                            GeoPoint ubi_coche = new GeoPoint(latitud, longitud);
+                            //add_marker_pospasajero(ubi_coche);
+                            add_marker_poscoche(ubi_coche,"Mi coche");
+                            //SharedPreferences preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+                            //SharedPreferences.Editor editor = preferences.edit();
+                            //editor.putString("last_pos", response.getString("longitud")+","+response.getString("latitud"));
+                            //editor.apply();
+
+                        } catch (JSONException e) {
+                            System.out.println("No hay coche");
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                //mTextViewResult.setText(error.getMessage());
+            }
+        });
+        mQueue.add(request);
+    }
 /*    void add_points_map(){
 
         //your items
@@ -137,14 +361,63 @@ public class mapa extends AppCompatActivity {
         map.getOverlays().add(mOverlay);
     }*/
 
-    void add_marker(GeoPoint startPoint){
-        Marker startMarker = new Marker(map);
-        startMarker.setPosition(startPoint);
-        //startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-        startMarker.setIcon(getResources().getDrawable(R.drawable.ic_vuelos));
-        startMarker.setTitle("Start point");
 
-        map.getOverlays().add(startMarker);
+    void add_marker_pospasajero(GeoPoint Point, String info){
+        Marker Marker = new Marker(map);
+        Marker.setPosition(Point);
+        //startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        Marker.setIcon(getResources().getDrawable(R.drawable.user));
+        Marker.setTitle(info);
+        map.getOverlays().add(Marker);
+    }
+
+    void add_marker_poscoche(GeoPoint Point, String info){
+        Marker Marker = new Marker(map);
+        Marker.setPosition(Point);
+        //startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        Marker.setIcon(getResources().getDrawable(R.drawable.car1));
+        Marker.setTitle(info);
+        map.getOverlays().add(Marker);
+    }
+
+    void add_marker_cart(GeoPoint Point, String info){
+        Marker Marker = new Marker(map);
+        Marker.setPosition(Point);
+        //startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        Marker.setIcon(getResources().getDrawable(R.drawable.cart));
+        Marker.setTitle(info);
+
+        map.getOverlays().add(Marker);
+    }
+
+    void add_marker_restaruant(GeoPoint Point, String info){
+        Marker Marker = new Marker(map);
+        Marker.setPosition(Point);
+        //startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        Marker.setIcon(getResources().getDrawable(R.drawable.restaurant));
+        Marker.setTitle(info);
+
+        map.getOverlays().add(Marker);
+    }
+
+    void add_marker_info(GeoPoint Point, String info){
+        Marker Marker = new Marker(map);
+        Marker.setPosition(Point);
+        //startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        Marker.setIcon(getResources().getDrawable(R.drawable.info));
+        Marker.setTitle(info);
+
+        map.getOverlays().add(Marker);
+    }
+
+    void add_marker_vip(GeoPoint Point, String info){
+        Marker Marker = new Marker(map);
+        Marker.setPosition(Point);
+        //startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        Marker.setIcon(getResources().getDrawable(R.drawable.vip));
+        Marker.setTitle(info);
+
+        map.getOverlays().add(Marker);
     }
 
     @Override
